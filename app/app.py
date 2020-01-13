cd5cb86ead509f05deb24c7c11abd33217ea4ea3
knigawkl <lukasz.knigawka@outlook.com>
2020-01-13 16:01:53 +0100

    adding login form

--- app/app.py
+++ app/app.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'jhbewfjobewqfoeif'
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+app.secret_key = 'your secret key'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'knigawa'
+app.config['MYSQL_DB'] = 'security4dummies'
+
+mysql = MySQL(app)
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    return render_template('index.html', msg='')
